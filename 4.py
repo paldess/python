f6a46d509eebb4a962fc8bf7ba8a8ba913b665a3
pavel <makovpavel@rambler.ru>
2021-04-19 19:47:23 +0300

    исправлена 4 задача(2^(-4)

--- 4.py
+++ 4.py
@@ -1,7 +1,9 @@
 def my_func(x, y):
     out = x
-    for _ in range(1, y):
+    for _ in range(1, abs(y)):
         out *= x
+    if y < 0:
+        out = 1/out
     return out
 while True:
     try:
